ced5607bfa95813f1dfb057503cfebfbb4759013
serfuse <serfuse@gmail.com>
2018-08-17 21:44:34 +0800

    Add Constants class for db connection information

---
+++ src/main/java/config/Constants.java
@@ -0,0 +1,7 @@
+package config;
+
+public final class Constants {
+    public static final String neo4j_uri = "bolt://localhost:7687";
+    public static final String neo4j_user = "neo4j";
+    public static final String neo4j_password = "123456";
+}
