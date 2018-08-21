365bd51cf5552d53111368cbd38bc46ab127eaf5
serfuse <serfuse@gmail.com>
2018-08-21 15:20:21 +0800

    Create `Comparator`s for cattle pojo

--- src/main/java/model/CattleComparator.java
+++ src/main/java/model/CattleComparator.java
@@ -13,21 +13,21 @@ public class CattleComparator {
         }
     };
 
-    Comparator<CattlePOJO> byBirthday = new Comparator<CattlePOJO>() {
+    public static Comparator<CattlePOJO> byBirthday = new Comparator<CattlePOJO>() {
         @Override
         public int compare(CattlePOJO o1, CattlePOJO o2) {
             return o1.getBirthday().compareTo(o2.getBirthday());
         }
     };
 
-    Comparator<CattlePOJO> byBirthdayDesc = new Comparator<CattlePOJO>() {
+    public static Comparator<CattlePOJO> byBirthdayDesc = new Comparator<CattlePOJO>() {
         @Override
         public int compare(CattlePOJO o1, CattlePOJO o2) {
             return o2.getBirthday().compareTo(o1.getBirthday());
         }
     };
 
-    Comparator<CattlePOJO> byWeight = new Comparator<CattlePOJO>() {
+    public static Comparator<CattlePOJO> byWeight = new Comparator<CattlePOJO>() {
         @Override
         public int compare(CattlePOJO o1, CattlePOJO o2) {
             return o1.getWeight() > o2.getWeight() ? 1 : o1.getWeight() == o2.getWeight() ? 0 : -1;
